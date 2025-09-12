7acd16d8709c1a5539e7e9812fba4caa0edb4713
ShahzebMahmood <shahzebmahmood3@gmail.com>
2025-09-11 20:46:10 -0400

    refractor: lint code

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,7 +1,7 @@
 resource "aws_eks_cluster" "this" {
   name     = var.cluster_name
   role_arn = var.cluster_role_arn
-  version  = "1.26"
+  version  = "1.33"  # Updated EKS version
 
   vpc_config {
     subnet_ids = var.subnet_ids
