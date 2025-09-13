cce25345a5f5a0ed9bb6d0621c82a2ea0f05f881
ShahzebMahmood <shahzebmahmood3@gmail.com>
2025-09-12 20:07:53 -0400

    feat: enhance secrets management module with OIDC provider integration and IAM role reference for improved security

--- modules/secrets/variables.tf
+++ modules/secrets/variables.tf
@@ -8,6 +8,16 @@ variable "pod_role_name" {
   type        = string
 }
 
+variable "oidc_provider_arn" {
+  description = "ARN of the OIDC provider for EKS"
+  type        = string
+}
+
+variable "oidc_provider_url" {
+  description = "URL of the OIDC provider for EKS"
+  type        = string
+}
+
 variable "tags" {
   description = "A map of tags to assign to the resources"
   type        = map(string)
