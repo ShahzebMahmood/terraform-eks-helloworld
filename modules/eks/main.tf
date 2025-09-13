eb0fd560007d669eb81c43651043f836c7d02731
ShahzebMahmood <shahzebmahmood3@gmail.com>
2025-09-13 19:19:38 -0400

    feat: Upgrade EKS cluster version from 1.29 to 1.34

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,7 +1,7 @@
 resource "aws_eks_cluster" "this" {
   name     = var.cluster_name
   role_arn = var.cluster_role_arn
-  version  = "1.29"
+  version  = "1.34"
 
   vpc_config {
     subnet_ids = var.subnet_ids
