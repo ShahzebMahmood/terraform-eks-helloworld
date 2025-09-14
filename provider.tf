1766f4ea8f21df0c46c358b87c2577f3e57ec281
ShahzebMahmood <shahzebmahmood3@gmail.com>
2025-09-13 22:33:14 -0400

    chore: Remove unnecessary whitespace in main.tf and provider.tf for cleaner code

--- provider.tf
+++ provider.tf
@@ -30,7 +30,7 @@ provider "aws" {
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority)
-  
+
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
